74010938df520ff3f7d2652d05fd1dd9fe87fc8f
Mohamed Ali <munnayesyou@gmail.com>
2018-09-17 08:58:05 +0530

    uname-fix

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("nettech_main","nettech_sasi","","XMQ5ROD_MNYq");
+$con = mysqli_connect("nettech_vehicle","nettech_sasi","","XMQ5ROD_MNYq");
     if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	die();
