ecf969e31dda86c7e5913f0407d667cebea7e129
Mohamed Ali <munnayesyou@gmail.com>
2018-09-17 09:02:16 +0530

    db details changed

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("nettech_vehicle","nettech_sasi","","XMQ5ROD_MNYq");
+$con = mysqli_connect("veh.nettech.cf","nettech_sasi","XMQ5ROD_MNYq","nettech_vehicle");
     if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	die();
