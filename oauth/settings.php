0330f8ba941d5f1cb43a60069871c7a3f41cd9f6
Mohamed Ali <munnayesyou@gmail.com>
2018-12-01 14:29:38 +0530

    vsm oauth new client secret code on settings.php

--- oauth/settings.php
+++ oauth/settings.php
@@ -4,7 +4,7 @@
 define('CLIENT_ID', '875688751535-oleiqa9e1f7joc74h3el1a5et4e9m0kt.apps.googleusercontent.com');
 
 /* Google App Client Secret */
-define('CLIENT_SECRET', 'zAibb9Cqhjz4iauzZIpQIbzP');
+define('CLIENT_SECRET', 'etHN5A0mMJJPt3WQ0llUvMSD');
 
 /* Google App Redirect Url */
 define('CLIENT_REDIRECT_URL', 'https://veh.nettech.ga/oauth/gauth.php');
