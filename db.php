76216ed481384ce61484a4de33b47e2c89672cdc
Mohamed Ali <munnayesyou@gmail.com>
2018-12-01 14:35:56 +0530

    db ga fix

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$connection = mysqli_connect("veh.nettech.cf","nettech_sasi","XMQ5ROD_MNYq","nettech_vehicle");
+$connection = mysqli_connect("veh.nettech.ga","nettech_sasi","XMQ5ROD_MNYq","nettech_vehicle");
     if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  $base_url = "http://veh.nettech.cf/mailverify/";
+  $base_url = "http://veh.nettech.ga/mailverify/";
 	die();
 	}
 ?>
